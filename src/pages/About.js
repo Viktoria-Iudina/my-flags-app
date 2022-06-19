a1c401b438baabc8b3ef0b545a9f6fac8132dcd7
Viktoria-Iudina <jajudinaja@gmail.com>
2022-06-19 10:07:15 +0200

    Added components

---
+++ src/pages/About.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis eos
+        deleniti, voluptates inventore atque mollitia quam. Consequuntur porro
+        placeat similique vel ullam nostrum maxime. Delectus nemo, aliquid
+        voluptas quia totam ut excepturi fugiat in earum quasi nesciunt, ipsum
+        qui obcaecati nisi omnis optio magnam animi maxime sapiente enim
+        deserunt officia molestiae aspernatur corrupti? Possimus, similique
+        corporis, debitis natus aspernatur deserunt illum veritatis
+        necessitatibus iste soluta molestias molestiae. Aperiam quia itaque ipsa
+        ducimus. Facilis veritatis harum reiciendis nisi exercitationem
+        temporibus eveniet placeat rerum repellat molestiae ipsam similique,
+        voluptates soluta alias at? Tenetur tempora quisquam consequatur numquam
+        corporis ipsam placeat, eaque expedita!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti quas
+        ad animi illo, sunt perferendis deserunt voluptatum nostrum quae fugiat
+        vel a eum esse totam molestias cum voluptate distinctio dolorum alias
+        dolor laboriosam odit dolores! Recusandae, modi laboriosam voluptates
+        illum quia cupiditate impedit eligendi vel, hic exercitationem dolore
+        quis ab?
+      </p>
+    </div>
+  );
+};
+
+export default About;
