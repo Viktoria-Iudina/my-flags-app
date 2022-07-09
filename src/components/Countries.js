211dc105e3d793ac05966c58ae4ecd0935ddeb10
Viktoria-Iudina <jajudinaja@gmail.com>
2022-07-09 10:45:58 +0200

    filtering countries by continent

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -40,9 +40,13 @@ const Countries = () => {
         ))}
       </ul>
       <ul className="countries-list">
-        {data.slice(0, rangeValue).map((country, index) => (
-          <Card key={index} country={country} />
-        ))}
+        {data
+          .filter((country) => country.continents[0].includes(selectedRadio))
+          .sort((a, b) => b.population - a.population)
+          .slice(0, rangeValue)
+          .map((country, index) => (
+            <Card key={index} country={country} />
+          ))}
       </ul>
     </div>
   );
