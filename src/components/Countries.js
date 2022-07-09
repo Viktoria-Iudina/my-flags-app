c1b24f5115ceb45e8c4cc5dbc05810fd38e260dc
Viktoria-Iudina <jajudinaja@gmail.com>
2022-07-09 09:50:58 +0200

    targeting selected continent with radios

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -5,6 +5,9 @@ import Card from "../components/Card";
 const Countries = () => {
   const [data, setData] = useState([]);
   const [rangeValue, setRangeValue] = useState(6);
+  //   targeting selected continent
+  const [selectedRadio, setSelectedRadio] = useState("");
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
   useEffect(() => {
     axios
@@ -20,8 +23,21 @@ const Countries = () => {
           min="1"
           max="250"
           defaultValue={rangeValue}
+          //   targeting selected continent
           onChange={(e) => setRangeValue(e.target.value)}
         />
+        {radios.map((continent) => (
+          <li>
+            <input
+              type="radio"
+              id={continent}
+              name="continentRadio"
+              //   targeting selected continent
+              onChange={(e) => setSelectedRadio(e.target.id)}
+            />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
+        ))}
       </ul>
       <ul className="countries-list">
         {data.slice(0, rangeValue).map((country, index) => (
